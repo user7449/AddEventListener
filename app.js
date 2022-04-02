a6c3e1f50ee14a6c8324db3e1fb78c5a7e78401d
user7449 <user.15.00@mail.ru>
2022-04-02 21:12:50 +0300

    AddEventListener in javascript language

---
+++ app.js
@@ -0,0 +1,21 @@
+let salam =document.getElementById("salam");
+let saqol =document.getElementById("saqol");
+
+saqol.style.display="none";
+
+
+
+let btn =document.getElementById("btn");
+
+btn.addEventListener("click",function(){
+    let saqol =document.getElementById("saqol");
+    saqol.style.display="block";
+
+btn.addEventListener("click",function(){
+    let saqol =document.getElementById("saqol");
+    saqol.style.display="none";
+})
+
+})
+
+
